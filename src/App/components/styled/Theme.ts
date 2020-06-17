54eb61b9dd2ac8ee418de32227c97e980f56ef11
subject026 <sudobuildspeakers@gmail.com>
2020-06-17 23:18:24 +0100

    theme

---
+++ src/App/components/styled/Theme.ts
@@ -0,0 +1,23 @@
+const Theme = {
+  colors: {
+    grey100: '#202020',
+    grey200: '#585858',
+    grey300: '#757575',
+    grey400: '#BDBDBD',
+    grey500: '#E2E2E2',
+    grey600: '#E9E9E9',
+    grey700: '#F3F3F3',
+    grey800: '#F8f8f8',
+  },
+  layout: {
+    base: '16px',
+    base10: '160px',
+  },
+  button: {
+    default: {
+      fontSize: '1rem',
+    },
+  },
+};
+
+export default Theme;
