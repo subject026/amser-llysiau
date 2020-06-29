280b3be75b84d0997d0afc0306d6bdb7347d0b7c
subject026 <sudobuildspeakers@gmail.com>
2020-06-30 00:22:28 +0100

    made wrapper flexible

--- src/App/components/styled/wrapper.ts
+++ src/App/components/styled/wrapper.ts
@@ -1,8 +1,18 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export default styled.div`
+type TWrapper = {
+  sm?: string;
+  md?: string;
+};
+
+export default styled.div<TWrapper>`
   width: 100%;
   max-width: 1000px;
   margin: auto;
-  padding: 0 15px;
+  ${(props) => css`
+    padding: 0 ${props.sm ? props.sm : 0};
+    @media (min-width: 699px) {
+      padding: 0 ${props.md ? props.md : 0};
+    }
+  `}
 `;
